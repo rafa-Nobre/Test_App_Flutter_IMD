e0207badf345c032121cc65f842e17acf5c34426
rafa-Nobre <denys.nobre@outlook.com>
2023-09-11 12:56:18 -0300

    Adicionando novos widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,62 +10,117 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Restaurant'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Restaurante'),
+          centerTitle: true,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(20),
+              bottomLeft: Radius.circular(20),
+            ),
+          ),
+        ),
+        drawer: Drawer(
+          child: SafeArea(
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              height: 100,
+              child: const Column(
+                children: <Widget>[Text('Olá, seja bem vindo!')],
+              ),
+            ),
+          ),
+        ),
+        body: const MyHomePage(),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String message = '';
+  int _pessoa = 0;
+  String _mensagem = "Pode entrar!";
 
-  void _incrementCounter() {
-    int pace = 1;
+  void _changePeople(int delta) {
     setState(() {
-      _counter += pace;
-      if (_counter > 0) {}
+      _pessoa += delta;
+      if (_pessoa >= 20) {
+        _mensagem = "Lotado, não pode entrar.";
+        _pessoa = 20;
+      } else if (_pessoa < 20 && _pessoa >= 0) {
+        _mensagem = "Pode entrar!";
+      } else {
+        _pessoa = 0;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title: Text(widget.title),
-      ),
-      body: Stack(children: <Widget>[
+    return Stack(
+      children: <Widget>[
         Image.asset(
-          'lib/assets/restaurante.jpg',
+          "lib/assets/restaurante.jpg",
           fit: BoxFit.cover,
           height: 1000.0,
         ),
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              "Pessoas: $_pessoa",
+              style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
+            ), //text
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ElevatedButton(
+                    child: const Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.black),
+                    ),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ElevatedButton(
+                    child: const Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.black),
+                    ),
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
+                  ),
+                ),
+              ],
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ]),
+              _mensagem,
+              style: const TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
+            ) //text
+          ], //widget
+        ) //Column
+      ],
     );
   }
 }
